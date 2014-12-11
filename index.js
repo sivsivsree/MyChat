dc9524bebc2472d506124b5b687f7c6200b106d2
Siv S <sivsivsree@gmail.com>
2014-12-12 02:16:45 +0530

    Id integrated

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var userCount = 0;
+var users = [];
+
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -12,10 +15,41 @@ app.get('/jq', function(req, res){
 });
 
 io.on('connection', function(socket){
+
+
+  socket.on("join", function(id){
+    console.log(users.indexOf(id));
+  	if(users.indexOf(id) == -1){
+
+  		userCount = users.push(id);
+	    io.emit('online', users.length  );
+	    console.log(users.length  + " person joined");
+	    console.log(users);
+
+  	}
+
+  });
+
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
+    
   });
+
+
+  socket.on('left', function(id){
+	   var u = users.indexOf(id);
+
+	   if(u != -1){
+	   	 users.splice(u, 1);
+	   	 io.emit('online', users.length );
+	   	 console.log(users.length +" people online");
+	   }
+	   
+  });
+
 });
+
+
 
 http.listen(process.env.PORT || 5000, function(){
   console.log('Server up and running on Heroku Port || 5000');
